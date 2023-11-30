275f516fc392a444a123427dca420ea50e0f722f
Farhanyousaf729 <fboby23@gmail.com>
2023-12-01 04:33:09 +0500

    nav position sticky

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <main className="">
-      <div className="w-[100%] py-2 px-10 bg-blue-500 ">
+    <main className="sticky top-[1px] shadow-lg">
+      <nav className="w-[100%] py-2 px-10 bg-blue-500 ">
         <img src="/favicon.ico" alt="" />
-      </div>
+      </nav>
     </main>
   )
 }
