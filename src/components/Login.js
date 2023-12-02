a7ce7cee28405757d1908f0a746f9a34ae57f9dd
NAVEED <naveedali8086@gmail.com>
2023-12-02 23:49:17 +0500

    - Fixed Dropdown UI issues - Reduced redundant code while hiding/showing password - Showing loader on forgot-password second screen

--- src/components/Login.js
+++ src/components/Login.js
@@ -43,81 +43,75 @@ const Login = ({ goToSection }) => {
     }
   }, [formData]);
   return (
-    <>
-      <p className="px-8 text-red-500">{error ? error : null}</p>
+      <>
+        <p className="px-8 text-red-500">{error ? error : null}</p>
 
-      <form className="p-8" onSubmit={handleSubmit}>
-        <label htmlFor="email" className="font-semibold">
-          Email
-        </label>
-        <input
-          id="email"
-          name="email"
-          type="email"
-          onChange={onChange}
-          value={formData.email}
-          className="block my-2 w-[100%] outline-none border-2 border-gray-300 rounded-sm p-2"
-          placeholder="Enter Your Email"
-          required
-        />
-        <label htmlFor="password" className="font-semibold">
-          Password
-        </label>
-
-        <div className="flex items-center border-2 border-gray-300 my-2  rounded-sm">
+        <form className="p-8" onSubmit={handleSubmit}>
+          <label htmlFor="email" className="font-semibold">
+            Email
+          </label>
           <input
-            id="password"
-            name="password"
-            type={showPassword ? "text" : "password"}
-            onChange={onChange}
-            value={formData.password}
-            className="block  w-[100%] outline-none border-r-2  p-2"
-            placeholder="Password"
-            required
+              id="email"
+              name="email"
+              type="email"
+              onChange={onChange}
+              value={formData.email}
+              className="block my-2 w-[100%] outline-none border-2 border-gray-300 rounded-sm p-2"
+              placeholder="Enter Your Email"
+              required
           />
-          <button className="px-3 cursor-pointer" type="button">
-            {showPassword ? (
-              <MdOutlineRemoveRedEye
-                type="button"
-                onClick={() => setShowPassword(false)}
-              />
-            ) : (
-              <FaRegEyeSlash
-                type="button"
-                onClick={() => setShowPassword(true)}
-              />
-            )}
-          </button>
-        </div>
+          <label htmlFor="password" className="font-semibold">
+            Password
+          </label>
 
-        <button
-          type="submit"
-          className="w-[100%] bg-cyan-700 text-white font-semibold rounded-sm mt-2 py-2 cursor-pointer  flex justify-center items-center"
-          disabled={isLoading}
-        >
-          Login
-          {isLoading && <RiLoader4Line className="animate-spin text-xl" />}
-        </button>
-        <div className="flex justify-between sm:justify-center pt-8 text-cyan-700">
+          <div className="flex items-center border-2 border-gray-300 my-2  rounded-sm">
+            <input
+                id="password"
+                name="password"
+                type={showPassword ? "text" : "password"}
+                onChange={onChange}
+                value={formData.password}
+                className="block  w-[100%] outline-none border-r-2  p-2"
+                placeholder="Password"
+                required
+            />
+            <button className="px-3 cursor-pointer" type="button" onClick={
+              () => {
+                setShowPassword(!showPassword)
+              }
+            }>
+              {showPassword ? (<MdOutlineRemoveRedEye/>) : (<FaRegEyeSlash/>)}
+            </button>
+          </div>
+
           <button
-            className=""
-            type="button"
-            onClick={() => {
-              navigate("/forgot-password");
-            }}
+              type="submit"
+              className="w-[100%] bg-cyan-700 text-white font-semibold rounded-sm mt-2 py-2 cursor-pointer  flex justify-center items-center"
+              disabled={isLoading}
           >
-            Forgot Password?
+            Login
+            {isLoading && <RiLoader4Line className="animate-spin text-xl" />}
           </button>
-          <button
-            type="button"
-            onClick={() => goToSection("signup")}
-            className="block sm:hidden"
-          >
-            Create Account
-          </button>
-        </div>
-      </form>
-    </>
+          <div className="flex justify-between sm:justify-center pt-8 text-cyan-700">
+            <button
+                className=""
+                type="button"
+                onClick={() => {
+                  navigate("/forgot-password");
+                }}
+            >
+              Forgot Password?
+            </button>
+            <button
+                type="button"
+                onClick={() => goToSection("signup")}
+                className="block sm:hidden"
+            >
+              Create Account
+            </button>
+          </div>
+        </form>
+      </>
   );
 };
 
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -6,73 +6,70 @@ import { RiLoader4Line } from "react-icons/ri";
 const Signup = ({ goToSection }) => {
   const [error, setError] = useState("");
   const [message, setMessage] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
-  const [inputType, setInputType] = useState({
-    password: "password",
-    password2: "password",
-  });
   const [formData, setFormData] = useState({
     name: "",
     email: "",
     phone: "",
-    organization: "",
+    organisation: "",
     password: "",
     confirmPassword: "",
   });
-  const organizationOptions = ["Sole-trader/Individual", "InSquare Fit"];
+  const organisationOptions = ["Sole-trader/Individual", "InSquare Fit"];
   const onChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
   const handle1 = () => {
     UserPool.signUp(
-      formData.email,
-      formData.password,
-      [
-        {
-          Name: "name",
-          Value: formData.name,
-        },
-        {
-          Name: "email",
-          Value: formData.email,
-        },
-        {
-          Name: "phone_number", // This is the attribute name for the phone number
-          Value: formData.phone, // Use the phone number from your form data
-        },
-        {
-          Name: "custom:organization",
-          Value: formData.organization,
-        },
-      ],
-      null,
-      (err, data) => {
-        if (err) {
-          setError(err.message);
-          setIsLoading(false);
-        } else {
-          console.log("Successfully signed up:", data);
-          if (data) {
-            goToSection("login");
+        formData.email,
+        formData.password,
+        [
+          {
+            Name: "name",
+            Value: formData.name,
+          },
+          {
+            Name: "email",
+            Value: formData.email,
+          },
+          {
+            Name: "phone_number", // This is the attribute name for the phone number
+            Value: formData.phone, // Use the phone number from your form data
+          },
+          {
+            Name: "custom:organisation",
+            Value: formData.organisation,
+          },
+        ],
+        null,
+        (err, data) => {
+          if (err) {
+            setError(err.message);
             setIsLoading(false);
-            setFormData({
-              name: "",
-              email: "",
-              phone: "",
-              organization: "",
-              password: "",
-              confirmPassword: "",
-            });
+          } else {
+            console.log("Successfully signed up:", data);
+            if (data) {
+              goToSection("login");
+              setIsLoading(false);
+              setFormData({
+                name: "",
+                email: "",
+                phone: "",
+                organisation: "",
+                password: "",
+                confirmPassword: "",
+              });
+            }
           }
         }
-      }
     );
   };
   const handleSubmit = (e) => {
     e.preventDefault();
     let message = {};
     const passwordRegex =
-      /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+        /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
 
     if (formData.name === "") {
       message.requireName = "Name is required";
@@ -84,10 +81,10 @@ const Signup = ({ goToSection }) => {
       message.requirePassword = "Password is required";
     } else if (!passwordRegex.test(formData.password)) {
       message.requirePassword =
-        "Password must contain at least 8 characters, 1 number, 1 special character, 1 uppercase letter, and 1 lowercase letter";
+          "Password must contain at least 8 characters, 1 number, 1 special character, 1 uppercase letter, and 1 lowercase letter";
     }
-    if (formData.organization === "") {
-      message.requireOrgnization = "Organization must";
+    if (formData.organisation === "") {
+      message.requireOrgnization = "Organisation must";
     }
     if (formData.password === "") {
       message.requireConfirmPassword = "Password is required";
@@ -102,12 +99,7 @@ const Signup = ({ goToSection }) => {
       handle1();
     }
   };
-  const ToggleInputType = (field) => {
-    setInputType((prevInputType) => ({
-      ...prevInputType,
-      [field]: prevInputType[field] === "password" ? "text" : "password",
-    }));
-  };
+
   useEffect(() => {
     if (message) {
       setMessage("");
@@ -117,146 +109,143 @@ const Signup = ({ goToSection }) => {
     }
   }, [formData]);
   return (
-    <>
-      <p className="px-8 text-red-500">{error ? error : null}</p>
-      <form className="p-8" onSubmit={handleSubmit}>
-        <label htmlFor="name" className="font-semibold">
-          Name
-        </label>
-        <input
-          id="name"
-          name="name"
-          type="text"
-          onChange={onChange}
-          value={formData.name}
-          className="block mt-2 w-[100%] outline-none border-2 border-gray-300 rounded-sm p-2"
-          placeholder="Enter Your Name"
-        />
-        <p className="text-xs text-red-500 mb-2">{message?.requireName}</p>
-        <label htmlFor="email" className="font-semibold">
-          Email
-        </label>
-        <input
-          id="email"
-          name="email"
-          type="email"
-          onChange={onChange}
-          value={formData.email}
-          className="block mt-2 w-[100%] outline-none border-2 border-gray-300 rounded-sm p-2"
-          placeholder="Enter Your Email"
-        />
-        <p className="text-xs text-red-500 mb-2">{message?.requireEmail}</p>
-        <label htmlFor="phone" className="font-semibold">
-          Phone Number
-        </label>
-        <input
-          id="phone"
-          name="phone"
-          type="tel"
-          value={formData.phone}
-          onChange={onChange}
-          className="block my-2 w-[100%] outline-none border-2 border-gray-300 rounded-sm p-2"
-          placeholder="Enter Your Phone Number"
-        />
-        <div className="">
-          <label htmlFor="organization" className="font-semibold">
-            Organization
+      <>
+        <p className="px-8 text-red-500">{error ? error : null}</p>
+        <form className="p-8" onSubmit={handleSubmit}>
+          <label htmlFor="name" className="font-semibold">
+            Name
           </label>
-          <select
-            onChange={onChange}
-            id="organization"
-            value={formData.organization}
-            name="organization"
-            className={` ${
-              formData.organization ? "text-black" : "text-[#a9a9a9]"
-            } block mt-2 w-[100%] outline-none border-2 border-gray-300 rounded-sm p-2`}
-          >
-            <option disabled value="" className="text-[#a9a9a9]">
-              Please choose one option
-            </option>
-            {organizationOptions.map((option, index) => {
-              return (
-                <option value={option} className="text-black" key={index}>
-                  {option}
-                </option>
-              );
-            })}
-          </select>
-        </div>
-        <p className="text-xs text-red-500 mb-2">
-          {message?.requireOrgnization}
-        </p>
-        <label htmlFor="password" className="font-semibold">
-          Password
-        </label>
-        <div className="flex items-center border-2 border-gray-300 mt-2 rounded-sm">
           <input
-            id="password"
-            name="password"
-            type={inputType.password}
-            onChange={onChange}
-            value={formData.password}
-            className="block  w-[100%] outline-none border-r-2  p-2"
-            placeholder="Password"
+              id="name"
+              name="name"
+              type="text"
+              onChange={onChange}
+              value={formData.name}
+              className="block mt-2 w-[100%] outline-none border-2 border-gray-300 rounded-sm p-2"
+              placeholder="Enter Your Name"
           />
-          <button className="px-3 cursor-pointer" type="button">
-            {inputType.password !== "password" ? (
-              <MdOutlineRemoveRedEye
-                onClick={() => ToggleInputType("password")}
-              />
-            ) : (
-              <FaRegEyeSlash onClick={() => ToggleInputType("password")} />
-            )}
-          </button>
-        </div>
-        <p className="text-xs text-red-500 mb-2">{message?.requirePassword}</p>
-        <label htmlFor="confirmPassword" className="font-semibold">
-          Re-Password
-        </label>
-        <div className="flex items-center border-2 border-gray-300 mt-2  rounded-sm">
+          <p className="text-xs text-red-500 mb-2">{message?.requireName}</p>
+          <label htmlFor="email" className="font-semibold">
+            Email
+          </label>
           <input
-            id="confirmPassword"
-            name="confirmPassword"
-            type={inputType.password2}
-            onChange={onChange}
-            value={formData.confirmPassword}
-            className="block  w-[100%] outline-none border-r-2  p-2"
-            placeholder="Re-Password"
+              id="email"
+              name="email"
+              type="email"
+              onChange={onChange}
+              value={formData.email}
+              className="block mt-2 w-[100%] outline-none border-2 border-gray-300 rounded-sm p-2"
+              placeholder="Enter Your Email"
           />
-          <button className="px-3 cursor-pointer" type="button">
-            {inputType.password2 !== "password" ? (
-              <MdOutlineRemoveRedEye
-                onClick={() => ToggleInputType("password2")}
-              />
-            ) : (
-              <FaRegEyeSlash onClick={() => ToggleInputType("password2")} />
-            )}
-          </button>
-        </div>
-        <p className="text-xs text-red-500 mb-2">
-          {message.requireConfirmPassword
-            ? message.requireConfirmPassword
-            : message.passwordNotMatch
-            ? message.passwordNotMatch
-            : null}
-        </p>
-        <button
-          type="submit"
-          className="w-[100%] bg-cyan-700 text-white font-semibold rounded-sm mt-2 py-2 cursor-pointer  flex justify-center items-center"
-          disabled={isLoading}
-        >
-          Creating Account
-          {isLoading && <RiLoader4Line className="animate-spin text-xl" />}
-        </button>
+          <p className="text-xs text-red-500 mb-2">{message?.requireEmail}</p>
+          <label htmlFor="phone" className="font-semibold">
+            Phone Number
+          </label>
+          <input
+              id="phone"
+              name="phone"
+              type="tel"
+              value={formData.phone}
+              onChange={onChange}
+              className="block my-2 w-[100%] outline-none border-2 border-gray-300 rounded-sm p-2"
+              placeholder="Enter Your Phone Number"
+          />
+          <div className=''>
+            <label htmlFor="organisation" className="font-semibold">
+              Organisation
+            </label>
+            <select
+                onChange={onChange}
+                id="organisation"
+                value={formData.organisation}
+                name="organisation"
+                className={` ${
+                    formData.organisation ? "text-black" : "text-[#a9a9a9]"
+                } block mt-2 w-[100%] outline-none border-2 border-gray-300 rounded-sm p-2`}
+            >
+              <option disabled value="" className="text-[#a9a9a9]">
+                Please choose one option
+              </option>
+              {organisationOptions.map((option, index) => {
+                return (
+                    <option value={option} className="text-black" key={index}>
+                      {option}
+                    </option>
+                );
+              })}
+            </select>
+          </div>
+          <p className="text-xs text-red-500 mb-2">
+            {message?.requireOrgnization}
+          </p>
+          <label htmlFor="password" className="font-semibold">
+            Password
+          </label>
+          <div className="flex items-center border-2 border-gray-300 mt-2 rounded-sm">
+            <input
+                id="password"
+                name="password"
+                type={showPassword ? "text" : "password"}
+                onChange={onChange}
+                value={formData.password}
+                className="block  w-[100%] outline-none border-r-2  p-2"
+                placeholder="Password"
+            />
+            <button className="px-3 cursor-pointer" type="button" onClick={
+              () => {
+                setShowPassword(!showPassword)
+              }
+            }>
+              {showPassword ? (<MdOutlineRemoveRedEye/>) : (<FaRegEyeSlash/>)}
+            </button>
 
-        <button
-          onClick={() => goToSection("login")}
-          className="mt-2 text-cyan-700 sm:hidden"
-        >
-          Already Register?
-        </button>
-      </form>
-    </>
+          </div>
+          <p className="text-xs text-red-500 mb-2">{message?.requirePassword}</p>
+          <label htmlFor="confirmPassword" className="font-semibold">
+            Re-Password
+          </label>
+          <div className="flex items-center border-2 border-gray-300 mt-2  rounded-sm">
+            <input
+                id="confirmPassword"
+                name="confirmPassword"
+                type={showPassword ? "text" : "password"}
+                onChange={onChange}
+                value={formData.confirmPassword}
+                className="block  w-[100%] outline-none border-r-2  p-2"
+                placeholder="Re-Password"
+            />
+            <button className="px-3 cursor-pointer" type="button" onClick={
+              () => {
+                setShowPassword(!showPassword)
+              }
+            }>
+              {showPassword ? (<MdOutlineRemoveRedEye/>) : (<FaRegEyeSlash/>)}
+            </button>
+          </div>
+          <p className="text-xs text-red-500 mb-2">
+            {message.requireConfirmPassword
+                ? message.requireConfirmPassword
+                : message.passwordNotMatch
+                    ? message.passwordNotMatch
+                    : null}
+          </p>
+          <button
+              type="submit"
+              className="w-[100%] bg-cyan-700 text-white font-semibold rounded-sm mt-2 py-2 cursor-pointer  flex justify-center items-center"
+              disabled={isLoading}
+          >
+            Creating Account
+            {isLoading && <RiLoader4Line className="animate-spin text-xl" />}
+          </button>
+
+          <button
+              onClick={() => goToSection("login")}
+              className="mt-2 text-cyan-700 sm:hidden"
+          >
+            Already Register?
+          </button>
+        </form>
+      </>
   );
 };
 
